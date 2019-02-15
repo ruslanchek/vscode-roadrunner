aec3d42263983ff990d60d9ee066a74c1655a7a8
Ruslan Shashkov <shashkov@icloud.com>
2019-02-15 21:18:36 +0200

    Icons and actions

---
+++ src/Provider.ts
@@ -0,0 +1,100 @@
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
+
+export class RoadrunnerProvider implements vscode.TreeDataProvider<Task> {
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    Task | undefined
+  > = new vscode.EventEmitter<Task | undefined>();
+  readonly onDidChangeTreeData: vscode.Event<Task | undefined> = this
+    ._onDidChangeTreeData.event;
+
+  constructor(private workspaceRoot: string) {}
+
+  refresh(): void {
+    this._onDidChangeTreeData.fire();
+  }
+
+  getTreeItem(element: Task): vscode.TreeItem {
+    return element;
+  }
+
+  getChildren(): Thenable<Task[]> {
+    if (!this.workspaceRoot) {
+      vscode.window.showInformationMessage("No tasks in empty workspace");
+      return Promise.resolve([]);
+    }
+
+    const packageJsonPath = path.join(this.workspaceRoot, "package.json");
+
+    if (this.pathExists(packageJsonPath)) {
+      return Promise.resolve(this.getTasks(packageJsonPath));
+    } else {
+      vscode.window.showInformationMessage("Workspace has no package.json");
+      return Promise.resolve([]);
+    }
+  }
+
+  /**
+   * Given the path to package.json, read all its dependencies and devDependencies.
+   */
+  private getTasks(packageJsonPath: string): Task[] {
+    if (this.pathExists(packageJsonPath)) {
+      const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+
+      return Object.keys(packageJson.scripts).map(
+        script => new Task(script, packageJson.scripts[script])
+      );
+    } else {
+      return [];
+    }
+  }
+
+  private pathExists(p: string): boolean {
+    try {
+      fs.accessSync(p);
+    } catch (err) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
+export class Task extends vscode.TreeItem {
+  constructor(
+    public readonly label: string,
+    public readonly command: vscode.Command
+  ) {
+    super(label);
+  }
+
+  get tooltip(): string {
+    return `${this.label}: ${this.command}`;
+  }
+
+  get description(): string {
+    return "running";
+  }
+
+  iconPath = {
+    light: path.join(
+      __filename,
+      "..",
+      "..",
+      "resources",
+      "light",
+      "dependency.svg"
+    ),
+    dark: path.join(
+      __filename,
+      "..",
+      "..",
+      "resources",
+      "dark",
+      "dependency.svg"
+    )
+  };
+
+  contextValue = "task";
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { RoadrunnerProvider } from "./Provider";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,17 +13,54 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "extension.helloWorld",
+  const refreshTasks = vscode.commands.registerCommand(
+    "roadrunner.refreshTasks",
     () => {
       // The code you place here will be executed every time your command is executed
 
       // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World!");
+      vscode.window.showInformationMessage("Refresh tasks");
     }
   );
 
-  context.subscriptions.push(disposable);
+  const restartAll = vscode.commands.registerCommand(
+    "roadrunner.restartAll",
+    () => {
+      // The code you place here will be executed every time your command is executed
+
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Restart all tasks");
+    }
+  );
+
+  const stopAll = vscode.commands.registerCommand("roadrunner.stopAll", () => {
+    // The code you place here will be executed every time your command is executed
+
+    // Display a message box to the user
+    vscode.window.showInformationMessage("Stop all tasks");
+  });
+
+  const stop = vscode.commands.registerCommand("roadrunner.stop", () => {
+    // The code you place here will be executed every time your command is executed
+
+    // Display a message box to the user
+    vscode.window.showInformationMessage("Stop task");
+  });
+
+  const start = vscode.commands.registerCommand("roadrunner.run", () => {
+    // The code you place here will be executed every time your command is executed
+
+    // Display a message box to the user
+    vscode.window.showInformationMessage(vscode.workspace.rootPath!);
+  });
+
+  context.subscriptions.push(refreshTasks, restartAll, stopAll, stop, start);
+
+  const roadrunnerProvider = new RoadrunnerProvider(vscode.workspace.rootPath!);
+
+  vscode.window.registerTreeDataProvider("roadrunnerTasks", roadrunnerProvider);
+
+  roadrunnerProvider.refresh();
 }
 
 // this method is called when your extension is deactivated
